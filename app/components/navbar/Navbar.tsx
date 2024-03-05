6424b1694155dec7f3e69844000bca5ca9c8490a
Mikiyas Getachhew <125805964+mikiyas999@users.noreply.github.com>
2024-03-05 07:32:20 +0300

    Update Navbar.tsx

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ interface Navbarprops {
 
 const Navbar: React.FC<Navbarprops> = ({ currentUser }) => {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
+    <div className="fixed w-full bg-white z-20 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div
